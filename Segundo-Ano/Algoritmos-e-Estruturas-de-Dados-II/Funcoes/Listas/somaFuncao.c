e000eb281b86506c20e9aa300d506a7c364e9b02
joaomarcosp-souza <joaomarcosp.sza@gmail.com>
2023-05-13 20:41:53 -0300

    Pequena alteração

--- Segundo-Ano/Algoritmos-e-Estruturas-de-Dados-II/Funcoes/Listas/somaFuncao.c
+++ Segundo-Ano/Algoritmos-e-Estruturas-de-Dados-II/Funcoes/Listas/somaFuncao.c
@@ -9,10 +9,10 @@ int main()
 {
     int num1, num2;
 
-    printf("Valor num1: ");
+    printf("Numero 1: ");
     scanf("%d", &num1);
 
-    printf("Valor num2: ");
+    printf("Numero 2: ");
     scanf("%d", &num2);
 
     printf("Resultado: %d\n", somar(num1, num2));
